ceae2f56f529340f916c66ab1097727db41aa0d1
kevin <kevinbrothers@outlook.com>
2026-09-13 17:25:32 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace IndidntUI
+{
+    partial class Indidnt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.convertIndentButton = new System.Windows.Forms.Button();
+            this.convertBracketButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(1, 1);
+            this.inputText.Multiline = true;
+            this.inputText.Name = "inputText";
+            this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.inputText.Size = new System.Drawing.Size(798, 389);
+            this.inputText.TabIndex = 0;
+            // 
+            // convertIndentButton
+            // 
+            this.convertIndentButton.Location = new System.Drawing.Point(579, 396);
+            this.convertIndentButton.Name = "convertIndentButton";
+            this.convertIndentButton.Size = new System.Drawing.Size(209, 42);
+            this.convertIndentButton.TabIndex = 1;
+            this.convertIndentButton.Text = "Convert To Indent";
+            this.convertIndentButton.UseVisualStyleBackColor = true;
+            this.convertIndentButton.Click += new System.EventHandler(this.convertIndentButton_Click);
+            // 
+            // convertBracketButton
+            // 
+            this.convertBracketButton.Location = new System.Drawing.Point(579, 444);
+            this.convertBracketButton.Name = "convertBracketButton";
+            this.convertBracketButton.Size = new System.Drawing.Size(209, 42);
+            this.convertBracketButton.TabIndex = 2;
+            this.convertBracketButton.Text = "Convert To Bracketed";
+            this.convertBracketButton.UseVisualStyleBackColor = true;
+            this.convertBracketButton.Click += new System.EventHandler(this.convertBracketButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(12, 466);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Indidnt v1.0 - Created By Kevin Brothers";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(308, 467);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(110, 20);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Indidnt Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Indidnt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertBracketButton);
+            this.Controls.Add(this.convertIndentButton);
+            this.Controls.Add(this.inputText);
+            this.Name = "Indidnt";
+            this.ShowIcon = false;
+            this.Text = "Indidnt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button convertIndentButton;
+        private System.Windows.Forms.Button convertBracketButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IndidntUI
+{
+    public partial class Indidnt : Form
+    {
+        public Indidnt()
+        {
+            InitializeComponent();
+        }
+
+        private void convertIndentButton_Click(object sender, EventArgs e)
+        {
+            inputText.Text = ConvertToIndentation(inputText.Text);
+        }
+        private void convertBracketButton_Click(object sender, EventArgs e)
+        {
+            inputText.Text = ConvertToBracketed(inputText.Text);
+        }
+
+        public static string ConvertToIndentation(string filepath)
+        {
+            // Read File and put into list
+            var fileLines = filepath.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
+
+            List<string> lines = new List<string>();
+
+            foreach (var line in fileLines)
+            {
+                lines.Add(line);
+            }
+
+            int spacesPerIndent = 1;
+
+            List<int> indents = new List<int>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                // Set whitespace lines to an indent of -1, so we can ignore them later
+                if (lines[i].Length == 0) { indents.Add(-1); continue; }
+
+                // Tab count check
+                string text = lines[i];
+                char search = ' ';
+
+                // Recalibrate convention
+                char firstChar = lines[i][0];
+                if (firstChar == ' ') { search = ' '; spacesPerIndent = 4; }
+                else if (firstChar == '\t') { search = '\t'; }
+                else { indents.Add(0); continue; }
+
+                // Search for number of indents
+                int count = 0;
+
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (text[j] != search)
+                    {
+                        break;
+                    }
+                    count++;
+                }
+
+                count /= spacesPerIndent;
+
+                // Add that number to the indents list
+                indents.Add(count);
+            }
+
+            // Rebuild file, ignoring brackets
+            List<string> fixedLines = new List<string>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (indents[i] == -1) { fixedLines.Add(lines[i]); continue; }
+
+                // If we trim the line, and all that is there is { or } then we just skip and go about our day
+                if (lines[i].Trim().Equals("}") || lines[i].Trim().Equals("{"))
+                {
+                    continue;
+                }
+                fixedLines.Add(lines[i]);
+            }
+
+            // Rebuild script
+            string returnlines = "";
+            for (int pr = 0; pr < fixedLines.Count; pr++)
+            {
+                returnlines += fixedLines[pr];
+                returnlines += "\r\n";
+            }
+            return returnlines;
+        }
+
+        public static string ConvertToBracketed(string filePath)
+        {
+            // Read File and put into list
+            var fileLines = filePath.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
+
+            List<string> lines = new List<string>();
+
+            foreach (var line in fileLines)
+            {
+                lines.Add(line);
+            }
+
+            int spacesPerIndent = 1;
+
+            List<int> indents = new List<int>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                // Set whitespace lines to an indent of -1, so we can ignore them later
+                if (lines[i].Length == 0) { indents.Add(-1); continue; }
+
+                // Tab count check
+                string text = lines[i];
+                char search = ' ';
+
+                // Recalibrate convention
+                char firstChar = lines[i][0];
+                if (firstChar == ' ') { search = ' '; spacesPerIndent = 4; }
+                else if (firstChar == '\t') { search = '\t'; }
+                else { indents.Add(0); continue; }
+
+                // Search for number of indents
+                int count = 0;
+
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (text[j] != search)
+                    {
+                        break;
+                    }
+                    count++;
+                }
+
+                count /= spacesPerIndent;
+
+                // Add that number to the indents list
+                indents.Add(count);
+            }
+
+            // Insert the brackets based on the change in indents
+            List<string> fixedLines = new List<string>();
+            int prevIndent = 0;
+            for (int i = 0; i < lines.Count; i++)
+            {
+                // Ignore whitespace lines
+                if (indents[i] == -1)
+                {
+                    fixedLines.Add(lines[i]);
+                    continue;
+                }
+
+                // If there is a higher level indentaion found
+                if (prevIndent < indents[i])
+                {
+                    string brackIndent = new string(' ', prevIndent * spacesPerIndent);
+                    fixedLines.Add(brackIndent + "{");
+                }
+
+                // If there is a lower level indentaion found
+                if (prevIndent > indents[i])
+                {
+                    if (indents[i] == 0)
+                    {
+                        fixedLines.Add("}");
+                    }
+                    else
+                    {
+                        int indentLayers = prevIndent - indents[i];
+
+                        for (int l = indentLayers; l > 0; l--)
+                        {
+                            string brackIndent = new string(' ', l * spacesPerIndent);
+                            fixedLines.Add(brackIndent + "}");
+                        }
+                    }
+                }
+
+                string lineIndent = new string(' ', indents[i] * spacesPerIndent);
+                fixedLines.Add(lineIndent + lines[i].Trim());
+                prevIndent = indents[i];
+            }
+
+            // Rebuild script
+            string returnlines = "";
+            for (int pr = 0; pr < fixedLines.Count; pr++)
+            {
+                returnlines += fixedLines[pr];
+                returnlines += "\r\n";
+            }
+            return returnlines;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+
+            System.Diagnostics.Process.Start("https://github.com/kevinbbrothers");
+        }
+    }
+}
